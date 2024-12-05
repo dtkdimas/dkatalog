2f861a723971c02e717dc20cacd5cfcce4b517af
Dimas Rizky M <dimasrizkym3@gmail.com>
2024-12-05 15:23:44 +0700

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <h1>{{ __('Dashboard') }}</h1>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item active" aria-current="page">{{ __('Dashboard') }}</li>
+                <li class="breadcrumb-item active" aria-current="page">{{ __('Dashboard test') }}</li>
             </ol>
         </nav>
     </x-slot>
