5cdf6cc04b2d7d7657732faeaf0123e029bc666c
Dimas Rizky M <dimasrizkym3@gmail.com>
2024-12-06 15:13:41 +0700

    style: correct typo dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
         <h1>{{ __('Dashboard') }}</h1>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item active" aria-current="page">{{ __('Dashboard test') }}</li>
+                <li class="breadcrumb-item active" aria-current="page">{{ __('Dashboard') }}</li>
             </ol>
         </nav>
     </x-slot>
